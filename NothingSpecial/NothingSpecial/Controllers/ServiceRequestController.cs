5a0990fb1240d31f5e21ac390d85e4e9e640ce09
Katy <ksipple1@student.cvtc.edu>
2019-05-01 14:48:29 -0500

    Master Everything

---
+++ NothingSpecial/NothingSpecial/Controllers/ServiceRequestController.cs
@@ -0,0 +1,100 @@
+﻿#define TRACE
+using NothingSpecial.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NothingSpecial.Controllers
+{
+    public class ServiceRequestController : Controller
+    {
+        // Create a OpenJobsContext object to communicate with the OpenJob model.
+        private OpenJobsContext db = new OpenJobsContext();
+
+        // GET: Service Request Form view
+        public ActionResult ServiceRequestForm()
+        {
+            // Return the view named ServiceRequestForm. For more info about how routing works, look at the RouteConfig.cs
+            // file
+            return View();
+        }
+
+        // POST: Service Request Form view
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // Bind is used to only pass an object with only the parameters included below.  
+        public ActionResult ServiceRequestForm([Bind(Include = "JobID,FirstName,LastName,Email,PhoneNumber,Date,Message")] OpenJobModel openJobModel)
+        {
+            // If the instance variables are OK'd with the model, then continue.  
+            if (ModelState.IsValid)
+            {
+
+                // The controller adds the filled up model object to the database. TODO: REMOVE THIS
+                // db.OpenJobs.Add(openJobModel);
+                // db.SaveChanges();
+
+                // Return to the ServiceRequestDetails page, and send in a filled up openJobModel object to the Redirect.
+                return RedirectToAction("ServiceRequestDetails", openJobModel);
+            }
+
+            return View(openJobModel);
+        }
+
+        // GET: Service Request Details view
+        public ActionResult ServiceRequestDetails(OpenJobModel openJobModel)
+        {
+            /* ********** Uncomment when the dummy email account is made **********
+             
+            // If the openJobModel is empty, throw an exception.
+            if (openJobModel == null)
+            {
+                throw new ArgumentNullException(nameof(openJobModel));
+            }
+            else {
+                
+                 
+                // Notes: https://stackoverflow.com/questions/26784366/how-to-send-email-from-mvc-5-application
+                // Setting the object member variables to local variables
+                var fullName = openJobModel.FirstName.ToString() + openJobModel.LastName.ToString();
+                var email = openJobModel.Email.ToString();
+                var phoneNumber = openJobModel.PhoneNumber.ToString();
+                var date = openJobModel.Date.ToString();
+                var message = openJobModel.Message.ToString();
+
+                // String format for message body
+                var body = "<h1>Lead details</h1><br><br> <p>{0}</p><br> <p>{1}</p><br> <p>{2}</p><br> <p>{3}</p><br> <p>{4}</p><br>";
+
+                // Creating a Smtp client object to send the email in
+                SmtpClient client = new SmtpClient();
+
+                // Create a new email to send through the client
+                MailMessage mailMessage = new MailMessage();
+
+                // Email to send the message to
+                mailMessage.To.Add("technicianleads@nothingspecial.com");
+
+                // Subject for the email message
+                mailMessage.Subject = "Service Request lead sent " + DateTime.Now.ToString();
+
+                // Body for the email message, containing the lead information
+                mailMessage.Body = string.Format(body, fullName, email, phoneNumber, date, message);
+
+                // Send the email
+                client.Send(mailMessage);
+            }
+             
+            */
+
+
+            // Return the view with the filled up openJobModel object.
+            return View(openJobModel);
+        }
+
+    }
+}
---
+++ NothingSpecial/NothingSpecial/Models/OpenJobModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NothingSpecial.Models
+{       
+    [Table("OpenJobs")]
+    public class OpenJobModel
+    {
+        // Setting up the class for the Open Jobs Database. This is used by the Entity Framework to build
+        // the Open Jobs Database, which will house all of the work orders data. The [StringLength()] and [Required]
+        // in the square brackets are used for input validation. However, this will not be the only line of defence against 
+        // bad (or dangerous) user input. 
+
+        // Client (Customer) data
+        [Key]
+        public int JobID { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        [Required]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "You must provide a phone number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        [StringLength(10000)]
+        public string Message { get; set; }
+        
+        // Technician data
+        [StringLength(100000)]
+        [Display(Name = "Tech Notes")]
+        public string TechNotes { get; set; }
+
+        public double Estimate { get; set; }
+
+        [Display(Name = "Final Cost")]
+        public double FinalCost { get; set; }
+
+        [Display(Name = "Work Complete")]
+        public bool WorkComplete { get; set; }
+
+        [Display(Name = "Asset Model")]
+        public string AssetModel { get; set; }
+
+        [Display(Name = "Asset Type")]
+        public string AssetType { get; set; }
+    }
+}
